6a84af6973c93bd9cfbd06a7149b4f33749a8282
sandunwijes <107408557+sandunwijes@users.noreply.github.com>
2022-12-25 19:47:22 +0530

    initialized socail links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import heroImage from '../assets/heroImage.png'
+import React from "react";
+import heroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div><img src={heroImage} alt="" /></div>
-  )
-}
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800  text-white"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full ">
+          <h2 className="text-4xl sm:text-6xl font-bold ">
+            {" "}
+            I am a Web Developer
+          </h2>
+          <p className="text-gray-400 py-4 max-w-md">
+            Web Developer with more than one year of experience in frontend web
+            development and hands-on experience in HTML, CSS and JavaScript.
+            Experience in JS frameworks (React.js), popular CSS libraries and
+            version control tools. Sound knowledge in software engineering
+            principles and JavaScript core concepts. Familiarity with Node.js,
+            backend frameworks(express), RESTful APIs and NoSQL DBs (Mongo DB).
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-700 to-blue-900 cursor-pointer" >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={heroImage}
+            alt="my pic"
+            className="rounded-full sm:rounded-3xl max-w-md mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,14 +15,14 @@ function Navbar() {
   return (
     <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Sandun</h1>
+        <h1 className="text-5xl font-signature ml-2">Sandun Wijesinghe</h1>
       </div>
       <ul className="hidden md:flex">
         {navLinks.map(({ id, name }) => {
           return (
             <li
               key={id}
-              className=" px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200"
+              className=" px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-110 duration-200"
             >
               {name}
             </li>
