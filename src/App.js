f4fee05691522dd6c985cdb08200c1faa28f4f87
sandunwijes <107408557+sandunwijes@users.noreply.github.com>
2022-12-26 23:29:38 +0530

    completed technology component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import Technologies from "./components/Technologies";
 
 
 function App() {
@@ -11,9 +13,13 @@ function App() {
     <div className="App">
       <Navbar/>
       <Home/>
-      <SocialLinks/>
+      
       <About/>
       <Portfolio/>
+      <Technologies/>
+      <Contact/>
+
+      <SocialLinks/>
     </div>
   );
 }
