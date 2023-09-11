1fcb5658dde71ec752719ddfba1302a8846c582a
Sandun Wijesinghe <sandunnw@gmail.com>
2023-09-11 20:11:28 +0530

    fix: updated resume

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,7 +50,7 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 text-white">
             Projects
           </p>
-          <p className="py-4">Check out some of my work right here</p>
+          <p className="py-4">Check out some of my works right here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
